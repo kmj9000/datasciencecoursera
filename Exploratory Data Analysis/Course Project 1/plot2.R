535f19c20484eadfc92d48b68b8a1be3be47a591
Lim Ming Jin Kenneth <kmj9000@hotmail.com>
2015-04-13 02:48:50 +0800

    Course 4 Project 1

---
+++ Exploratory Data Analysis/Course Project 1/plot2.R
@@ -0,0 +1,20 @@
+setwd("~/datasciencecoursera/Exploratory Data Analysis/Course Project 1")
+
+## Load dataset into R
+data_all <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Subset the dataset to the range required
+data <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_all)
+
+## Convert dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
